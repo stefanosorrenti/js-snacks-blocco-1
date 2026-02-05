89e272bb4af327e2a4fcf1fc5b1122445f9ba99a
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-05 13:10:11 +0100

    complete snack-4

--- snack-4/main.js
+++ snack-4/main.js
@@ -14,27 +14,29 @@ se è dispari inseriscilo nell’array.*/
     -SE IL NUEMERO è  CHIESTO  è DISPARI (%)
         -PUSHO NELL'ARRAY (PUSH)
     -ALTRIMENTI
-        -NON LO PUSHO/ RIMUOVO (VEDIAMO)
+        -RIMUOVO IL NUMERO DIGITATO DALL'UTENTE
 
 
 
 */
 
- let oddNumbers = []
+ let oddNumbers = [] //creo un array vuoto (ARRAY)
 
  for (i = 0; i < 6; i++) {
-    const usersNumbers = parseInt(prompt('Scrivi il tuo numero.'))
+    const usersNumbers = parseInt(prompt('Scrivi il tuo numero.')) //chiedo per 6 volte un numero di inserire un numero
 
-    if (usersNumbers % 2 === 1) {
-        oddNumbers.push(usersNumbers)
+    if (usersNumbers % 2 === 1) { //SE IL NUEMERO è  CHIESTO  è DISPARI 
+        oddNumbers.push(usersNumbers) //PUSHO NELL'ARRAY 
         
     
+ }else { //ALTRIMENTI
+    oddNumbers.splice(usersNumbers, 1) //RIMUOVO IL NUMERO DIGITATO DALL'UTENTE
  }
 
-console.log(oddNumbers);
 }
 
-
+console.log(oddNumbers);
+ 
 
 /*
 for (let index = 0; index < usersNumbers.length; index++) {
