f83e20be42258b7d55c2a48176b641ef57d01307
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-05 12:54:59 +0100

    Add human code and first cicle

---
+++ snack-4/main.js
@@ -0,0 +1,41 @@
+//TRACCIA
+
+/*Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array.*/
+
+
+/*Cosa devo fare?
+
+
+-creo un array vuoto (ARRAY)
+-chiedo per 6 volte un numero di inserire un numero
+
+    -SE IL NUEMERO è  CHIESTO  è DISPARI (%)
+        -PUSHO NELL'ARRAY (PUSH)
+    -ALTRIMENTI
+        -NON LO PUSHO/ RIMUOVO (VEDIAMO)
+
+
+
+*/
+
+ let oddNumbers = []
+
+ for (i = 0; i < 6; i++) {
+    const usersNumbers = parseInt(prompt('Scrivi il tuo numero.'))
+ }
+
+
+
+
+
+/*
+for (let index = 0; index < usersNumbers.length; index++) {
+    const oddNumbers = usersNumbers[index];
+    if (oddNumbers % 2 === 1) {
+        usersNumbers.push(index)
+        break
+    }
+}
+*/
