1b49dc050358859bb196e6f22640cfa59c9ffa02
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-05 11:39:20 +0100

    General visual fix

--- snack-2/script-2.js
+++ snack-2/script-2.js
@@ -20,23 +20,30 @@
 
 //CHIEDO LA PRIMA PAROLA (PROMPT)
 const firstWord = prompt('Scrivi una parola');
+
 //structure
 console.log(`Questa è la prima parola: ${firstWord}`);
 
 
 //CHIEDO LA SECONDA PAROLA (PROMPT)
 const secondWord = prompt('Scrivi un altra parola');
+
 //structure
 console.log(`Questa è la seconda parola: ${secondWord}`);
 
 //SE LA PRIMA PAROLA è PIU' LUNGA DELLA PRIMA
 if (firstWord.length > secondWord.length) {
+
     console.log(`Questa è la parola più lunga:  ${firstWord}`); //stampo la prima parola
+
  //ALTRIMENTI SE LA SECONDA PAROLA è PIU' LUNGA DELLA PRIMA   
 } else if (secondWord.length > firstWord.length) {
+
     console.log(`Questa è la parola più lunga:  ${secondWord}`); //stampo la seconda parola
+
 //ALTRIMENTI
 } else {
+    
     console.log('La lunghezza dei caratteri è uguale'); //stampo (La lunghrzza della parola è uguale)
     
 }
